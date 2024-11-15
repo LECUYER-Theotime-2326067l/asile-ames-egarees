19c789ae3bed343a63db607b289425e7f00afad2
mathieu <mathieu.gravoille@etu.univ-amu.fr>
2024-11-15 14:33:32 +0100

    getters et setters

--- src/creatures/Creatures.java
+++ src/creatures/Creatures.java
@@ -4,16 +4,69 @@ import java.util.ArrayList;
 
 public class Creatures {
     private String name;
-    private boolean sex;
+    private String sex;
     private double weight;
     private double height;
     private int age;
     private int moralIndicator;
     private ArrayList<Sickness> sicknessList = new ArrayList<Sickness>();
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public int getMoralIndicator() {
+        return moralIndicator;
+    }
+
+    public ArrayList<Sickness> getSicknessList() {
+        return sicknessList;
+    }
+
+    public void setSicknessList(ArrayList<Sickness> sicknessList) {
+        this.sicknessList = sicknessList;
+    }
+
+    public void setMoralIndicator(int moralIndicator) {
+        this.moralIndicator = moralIndicator;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public String setSex(String sex) {
+        return this.sex = sex;
+    }
+
     public void waiting(){
         this.moralIndicator--;
     }
+
     public boolean screaming(){
         boolean screaming = false;
         if(this.moralIndicator == 0){
