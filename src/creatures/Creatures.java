5bfdf654758921d8944b0e7a5bcfa09f6e9ebdd8
mathieu <mathieu.gravoille@etu.univ-amu.fr>
2024-11-15 13:57:40 +0100

    start

---
+++ src/creatures/Creatures.java
@@ -0,0 +1,35 @@
+package creatures;
+
+import java.util.ArrayList;
+
+public class Creatures {
+    private String name;
+    private boolean sex;
+    private double weight;
+    private double height;
+    private int age;
+    private int moralIndicator;
+    private ArrayList<String> sicknessList = new ArrayList<String>();
+
+    public void waiting(){
+
+    }
+    public void screaming(){
+
+    }
+    public void getAngry(){
+
+    }
+    public void getSickness(){
+
+    }
+    public void loseSickness(){
+
+    }
+    public String getName(){
+        return name;
+    }
+    public void die(){
+
+    }
+}
