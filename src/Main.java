04855b2f52fedceb273d491cf90dff865d3e6cc5
mathieu <mathieu.gravoille@etu.univ-amu.fr>
2024-12-03 23:40:12 +0100

    Saisie clavier dans MAIN pour le Doctor

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,24 @@
 import DonneNotable.*;
 import creatures.Creature;
 import creatures.Dwarf;
+import hospital.Doctor;
 import hospital.Hospital;
 import hospital.MedicalService;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Veuillez entrer le nom du Doctor : ");
+        String name = scanner.nextLine();
+        System.out.println("Veuillez entrer son âge : ");
+        int age = Integer.parseInt(scanner.nextLine());
+        System.out.println("Veuillez entrer son genre : ");
+        String gender = scanner.nextLine();
+        Doctor doctor = new Doctor(name, age, gender);
+        System.out.println(doctor);
+
         Hospital hospital = new Hospital("nom", 10);
         String sex = Sex.generateSex();
         Creature creature = new Dwarf(Name.generateName(sex), sex, Weight.generateWeight("Dwarf"), Size.generateSize("Dwarf"), Age.generateAge("Dwarf"));
